4039960c86209e43e4bebcf45b1135b017280f03
Deivid Duarte <deivid.duarte@picpay.com>
2022-10-10 13:42:09 +0000

    Update main and module and variables

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
+/*
+locals {
+  sg-id = module.sg-tf.sg-tf
+}
+*/
 module "ec2-grafana" {
-  source                 = "./modules/ec2"
-  
+  source = "./modules/ec2"
+  sg-tf  = module.sg-tf.sg-tf
 }
 
 module "sg-tf" {
--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "web" {
   instance_type          = var.inst_type
   key_name               = var.inst_key
   user_data              = file("modules/ec2/grafana.sh")
+  vpc_security_group_ids = [var.sg-tf]
 
   tags = {
     Name = "DevOps"
